84a265b3f1022c52b0417dab546ed09e318cda12
nowic <nowichkoff.romi4@gmail.com>
2023-08-22 19:01:51 +0300

    minor changes

--- spyral-flip-game/frontend/shared/components/button-component/button.js
+++ spyral-flip-game/frontend/shared/components/button-component/button.js
@@ -1,29 +1,23 @@
 window.addEventListener("load", function () {
     let svgObject = document.getElementById('spyral-svg').contentDocument;
     let svg = svgObject.getElementById('spyral-logo');
-    console.log(svg);
+    // console.log(svg);
 
     let buttonsTopCollection = document.getElementsByClassName("button-top")
     for (let i = 0; i < buttonsTopCollection.length; i++) {
         buttonsTopCollection[i].addEventListener("mouseover", buttonTopMouseover, false)
         buttonsTopCollection[i].addEventListener("mouseout", buttonTopOnMouseout, false)
-        console.log("added eventlistener");
     }
 
     function buttonTopMouseover() {
+        svg.style.fill = "#354b74"
         // svg.querySelector("style").innerHTML = ".st0{fill:#FFFFFF;stroke:#000000;stroke-miterlimit:10;}.st1{fill:#354B74;}"
         // for non-optimized svg
-        console.log("triggered hover event");
     }
 
     function buttonTopOnMouseout() {
+        svg.style.fill = "#fff"
         // svg.querySelector("style").innerHTML = ".st0{fill:#FFFFFF;stroke:#000000;stroke-miterlimit:10;}.st1{fill:#FFFFFF;}"
         // for non-optimized svg
-        console.log("triggered unhover event");
     }
 })
-
-
-
-
-
